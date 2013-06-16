d4f40502cc31a681f77d7fb00bde84a16797932f
malejandro <manuedit@gmail.com>
2013-06-16 13:02:47 -0400

    Final

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Virtual-DJ-BPM-add-to-ID3-Utility
 =================================
 
 Script Python que extrae los BPM de las canciones en la base de datos de Virtual DJ, guardándolos en el campo BPM del archivo MP3.
+
+## Uso:
+
+python main.py [parametros]
+
+    --database -d:  Especifica la ruta al XMl con la base de datos de VirtualDJ
+    --start -s:     El track desde el cual se comienza a analizar
+    --tracks -t:    Cantidad de tracks a analizar
--- main.py
+++ main.py
@@ -9,28 +9,35 @@ reload(sys)
 class MuestraBpmManejador(ContentHandler):
     #Se llama al empezar a parsear
     def __init__(self):
-        self.indice = 0
+        self.indice = 1
         self.titulo = ""
         self.bpmVdj = "0"
         self.bpmTag = "0"
         self.esMp3 = 0
         self.contadorErrorGeneral = 0
-        self.errno2Archivos = 0
-        self.errno13Archivos = 0
+        self.errno2ArchivosName = ()
+        self.errno13ArchivosName = ()
+
         self.start = 0
         self.tracks = 0
+        self.analizados = 0
+        self.reporteTagCreados = 0
+        self.reporteGuardadoDesdeVDJ = 0
+        self.reporteReemplazadoDesdeVDJ = 0
+
+        self.debugMode = False
  
     #Se llama al encontrar una etiqueta de inicio de elemento <elemento>
     def startElement(self, name, attrs):
             #Entramos en Song
-            if name == 'Song' and self.indice < self.tracks:
+            if name == 'Song' and self.indice >= self.start and self.indice <= (self.tracks + self.start - 1):
                 #Se guarda variable sobre si song es mp3
                 self.esMp3 = attrs.get('FilePath').count("mp3")
                 #Se guarda variable con ruta del mp3
                 self.titulo = attrs.get('FilePath')
-                print "\n" + self.titulo.encode('cp850','replace')
+                if self.debugMode: print "\n" + self.titulo.encode('cp850','replace')
             #Entramos en BPM de Song, solo si el indice es menor que la cantidad predefinida y si es mp3
-            if name == 'BPM' and self.indice < self.tracks and self.esMp3 > 0:
+            if name == 'BPM' and self.esMp3 > 0:
                 if attrs.get('Bpm'):
                     #Se guarda el bpm de VDJ solo si ya existia
                     self.bpmVdj = "{0:.4f}".format(round(44100 / float(attrs.get('Bpm')) * 60, 4))
@@ -38,19 +45,20 @@ class MuestraBpmManejador(ContentHandler):
     def endElement(self, name):
         #Salimos del elemento song
         try:
-            if name == 'Song' and self.indice < self.tracks and self.esMp3 > 0:
+            if name == 'Song' and self.esMp3 > 0:
                 
-                print "BPM VDJ " + (self.bpmVdj).rjust(17)
+                if self.debugMode: print "BPM VDJ " + (self.bpmVdj).rjust(17)
                 #a leer el tag
                 if tag.link(self.titulo):
                     #guarda en var tagbpm del mp3 PARA MOSTRAR solo si este tiene tagbpm
                     if tag.getBPM():
                         self.bpmTag = "{0:.4f}".format(tag.getBPM())
-                        print "BPM TAG ORIGINAL " + (self.bpmTag).rjust(8)
+                        if self.debugMode: print "BPM TAG ORIGINAL " + (self.bpmTag).rjust(8)
                     else:
-                        print "ARCHIVO SIN BPM TAG PERO CON ID3TAG"
+                        if self.debugMode: print "ARCHIVO SIN BPM TAG PERO CON ID3TAG"
                 else:
-                    print "NO ID3TAG, CREANDO"
+                    if self.debugMode: print "NO ID3TAG, CREANDO"
+                    self.reporteTagCreados += 1
                     #creamos el tag
                     tag.header.setVersion(eyeD3.ID3_V2_3)
 
@@ -63,51 +71,77 @@ class MuestraBpmManejador(ContentHandler):
                         tag.setBPM(self.bpmVdj)
                         #se guarda lo cambiado
                         tag.update()
-                        print "GUARDADO DESDE BPMVDJ AL NO HABER PREVIO EN TAG"
-                        print "BPM OK: " + "{0:.4f}".format(tag.getBPM()).rjust(17)
+                        self.reporteGuardadoDesdeVDJ += 1
+                        if self.debugMode: print "GUARDADO DESDE BPMVDJ AL NO HABER PREVIO EN TAG"
+                        if self.debugMode: print "BPM OK: " + "{0:.4f}".format(tag.getBPM()).rjust(17)
                     else:
                         #si ya tiene un bpmtag previo, se compara con el de bpmVdj, si son iguales no se realiza nada, distintos, se guarda el de VDJ
                         if tag.getBPM() != float(self.bpmVdj):
                             tag.setBPM(self.bpmVdj)
                             #se guarda lo cambiado
                             tag.update()
-                            print "GUARDADO DESDE BPMVDJ AL SER DISTINTO A TAG"
-                            print "BPM OK: " + "{0:.4f}".format(tag.getBPM()).rjust(17)
+                            self.reporteReemplazadoDesdeVDJ += 1
+                            if self.debugMode: print "GUARDADO DESDE BPMVDJ AL SER DISTINTO A TAG"
+                            if self.debugMode: print "BPM OK: " + "{0:.4f}".format(tag.getBPM()).rjust(17)
                 else:
-                    print "NO HAY BPMVDJ, NO SE GUARDA NADA"
+                    if self.debugMode: print "NO HAY BPMVDJ, NO SE GUARDA NADA"
                 #El indice aumenta
                 self.indice += 1
+                self.analizados += 1
+
                         
                 #limpiar variables
                 self.titulo = ""
                 self.bpmVdj = "0"
                 self.bpmTag = 0
                 self.esMp3 = 0
+            elif name == 'Song':
+                self.indice += 1
         except IOError, e:
             if e.errno == 2:
-                print "ERROR: El archivo no se encuentra en la ruta:\n" + self.titulo.encode('cp850','replace')
+                self.errno2ArchivosName = self.errno2ArchivosName + (self.titulo.encode('cp850','replace'),)
+                if self.debugMode: print "ERROR: El archivo no se encuentra en la ruta:\n" + self.titulo.encode('cp850','replace')
             elif e.errno == 13:
-                print "ERROR: Se denegaron permisos para escribir en el archivo:\n" + self.titulo.encode('cp850','replace')
+                self.errno13ArchivosName = self.errno13ArchivosName + (self.titulo.encode('cp850','replace'),)
+                if self.debugMode: print "ERROR: Se denegaron permisos para escribir en el archivo:\n" + self.titulo.encode('cp850','replace')
             else:
                 raise e
         except Exception, e:
             print "ERROR " + str(e)
             raise e
-    def setInicio(self,nStart):
+    def setInicio(self, nStart):
         self.start = nStart
-    def setTracks(self,nTracks):
+    def setTracks(self, nTracks):
         self.tracks = nTracks
+    def setDebug(self, debug):
+        self.debugMode = debug
+    def endDocument(self):
+        print "\n\nResultado de ejecucion:"
+        print "\tAnalizados exitosamente:\t\t" + str(self.analizados)
+        print "\tId3tag creados:\t\t\t\t" + str(self.reporteTagCreados)
+        print "\tGuardado desde VDJ BPM:\t\t\t" + str(self.reporteGuardadoDesdeVDJ)
+        print "\tReemplazado ID3TAG BPM desde VDJ BPM:\t" + str(self.reporteReemplazadoDesdeVDJ)
+
+        print "\n\nErrores:"
+        print "\tNo encontrados:\t\t\t\t" + str(len(self.errno2ArchivosName))
+        for arch in self.errno2ArchivosName:
+            print "\t\t" + str(arch)
+
+        print "\tSin permiso de escritura:\t\t" + str(len(self.errno13ArchivosName))
+        for arch in self.errno13ArchivosName:
+            print "\t\t" + str(arch)
+
 
 def usage():
-    print "Uso:\n--database: Especifica la ruta al XMl con la base de datos de VirtualDJ\n--start: El track desde el cual se comienza a analizar\n--tracks: Cantidad de tracks a analizar"
+    print "Uso:\n\t--database -d:\tEspecifica la ruta al XMl con la base de datos de VirtualDJ\n\t--start -s:\tEl track desde el cual se comienza a analizar\n\t--tracks -t:\tCantidad de tracks a analizar"
 
 def main(argv):
     try:
         db = ''
         start = 1
         tracks = 1000
-
-        opts, args = getopt.getopt(argv, "d:s:t:", ["database=", "start=", "tracks="])
+        debug = False
+        opts, args = getopt.getopt(argv, "d:s:t:p", ["database=", "start=", "tracks=", "partyMode"])
         
         if len(opts) == 0:
             raise Exception("No se han ingresado argumentos validos")
@@ -119,14 +153,18 @@ def main(argv):
                 start = int(arg)
             elif opt in ("-t", "--tracks"):
                 tracks = int(arg)
-        print "Analizando base de datos ubicada en: " + db
+            elif opt in ("-p", "--partyMode"):
+                debug = True
+        print "\nAnalizando base de datos ubicada en: " + db
         print "Desde el track numero: " + str(start)
         print "Tracks a analizar: " + str(tracks)
+        print "Analizando..."
 
         parser = make_parser()
         handler = MuestraBpmManejador()
         handler.setInicio(start)
         handler.setTracks(tracks)
+        handler.setDebug(debug)
         parser.setContentHandler(handler)
         
         #Parseamos el fichero
